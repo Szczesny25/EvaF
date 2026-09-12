f8787d23b41a4ef2fe6618d5c862219782ac0460
Szczesny25 <andre.secall.munoz@estudiante.ipss.cl>
2026-09-11 21:22:44 -0300

    Tamo readi con la base de datos

---
+++ database/migrations/2026_09_11_233225_create_usuarios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('rut', 12)->unique();
+            $table->string('nmbre', 100);
+            $table->string('apellido', 100);
+            $table->string('correo', 150)->unique();
+            $table->string('contraseña', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
